9b362da76fd41b7b029149844f1332b37a0e1ccd
Obyashi <117147995+Obyashi@users.noreply.github.com>
2023-01-07 11:53:22 +0900

    aa

--- sample.py
+++ sample.py
@@ -1,50 +1,54 @@
-from flask import Flask, request, abort
+from django.http import HttpResponseForbidden, HttpResponse
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, FollowEvent, UnfollowEvent,
+    TextSendMessage, ImageMessage, AudioMessage
 )
-import os
 
-app = Flask(__name__)
+from linebot import LineBotApi, WebhookHandler
 
-YOUR_CHANNEL_ACCESS_TOKEN = "YOUR_CHANNEL_ACCESS_TOKEN"
-YOUR_CHANNEL_SECRET = "YOUR_CHANNEL_SECRET"
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+# 各クライアントライブラリのインスタンス作成
+line_bot_api = LineBotApi(channel_access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(channel_secret=LINE_ACCESS_SECRET)
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
+def callback(request):
+    # signatureの取得
+    signature = request.META['HTTP_X_LINE_SIGNATURE']
+    body = request.body.decode('utf-8')
     try:
+        # 署名の検証を行い、成功した場合にhandleされたメソッドを呼び出す
         handler.handle(body, signature)
     except InvalidSignatureError:
-        abort(400)
+        return HttpResponseForbidden()
+    return HttpResponse('OK')
 
-    return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+# フォローイベントの場合の処理
+@handler.add(FollowEvent)
+def handle_follow(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='初めまして')
+    )
 
 
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# メッセージイベントの場合の処理
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    # メッセージでもテキストの場合はオウム返しする
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+@handler.add(MessageEvent, message=(ImageMessage, AudioMessage))
+def handle_image_audio_message(event):
+    # 画像と音源の場合は保存する
+    content = line_bot_api.get_message_content(event.message.id)
+    with open('file', 'w') as f:
+        for c in content.iter_content():
+            f.write(c)
