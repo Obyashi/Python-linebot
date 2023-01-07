e5fa7a5ab54b4c7b3f6855c4eb9269443bc80513
Obyashi <117147995+Obyashi@users.noreply.github.com>
2023-01-07 13:29:50 +0900

    aa

--- sample.py
+++ sample.py
@@ -1,46 +1,54 @@
-#!/usr/bin/python
-# -*- coding: Shift-JIS -*-
-from django.http import HttpResponseForbidden, HttpResponse
 
-from linebot.exceptions import InvalidSignatureError
+from flask import Flask, request, abort
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import (
-    MessageEvent, TextMessage, FollowEvent, UnfollowEvent,
-    TextSendMessage, ImageMessage, AudioMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
-from linebot import LineBotApi, WebhookHandler
+app = Flask(__name__)
 
-line_bot_api = LineBotApi(channel_access_token="YOUR_CHANNEL_ACCESS_TOKEN")
-handler = WebhookHandler(channel_secret="YOUR_CHANNEL_SECRET")
+#ä¬ã´ïœêîéÊìæ
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-def callback(request):
-    signature = request.META['HTTP_X_LINE_SIGNATURE']
-    body = request.body.decode('Shift-JIS')
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        return HttpResponseForbidden()
-    return HttpResponse('OK')
+        abort(400)
 
-
-@handler.add(FollowEvent)
-def handle_follow(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='èâÇﬂÇ‹ÇµÇƒ')
-    )
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
+def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+        TextSendMessage(text=event.message.text))
 
-@handler.add(MessageEvent, message=(ImageMessage, AudioMessage))
-def handle_image_audio_message(event):
-    content = line_bot_api.get_message_content(event.message.id)
-    with open('file', 'w') as f:
-        for c in content.iter_content():
-            f.write(c)
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
